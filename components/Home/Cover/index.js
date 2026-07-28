59927c2fd5c5b5a256ba0e971994329e565b1dfe
ChiVinchiEgbo <egbovincent005@gmail.com>
2026-07-28 18:50:18 -0400

    fix: resolve Promise timeout rejections in course.js and add unoptimized to Cover image

--- components/Home/Cover/index.js
+++ components/Home/Cover/index.js
@@ -3,5 +3,5 @@ import React from 'react'
 
 export default function Cover(props) {
   const src = props.imageUrl || 'https://firebasestorage.googleapis.com/v0/b/web3dev-bootcamp.appspot.com/o/courses_cover%2FSolana_NFTs.png?alt=media&token=fcdba884-7e66-46b8-9282-fced339907da'
-  return <Image width={350} height={350} src={src} className="rounded-3xl" />
+  return <Image width={350} height={350} src={src} className="rounded-3xl" unoptimized={true} alt="Course cover" />
 }
