86104397bd52732173136026a5741f7ee4046cc9
ChiVinchiEgbo <egbovincent005@gmail.com>
2026-07-28 07:17:33 -0400

    fix: remove useMemo state update anti-pattern and add fallback image in Home component

--- components/Home/Cover/index.js
+++ components/Home/Cover/index.js
@@ -2,5 +2,6 @@ import Image from 'next/image'
 import React from 'react'
 
 export default function Cover(props) {
-  return <Image width={350} height={350} src={props.imageUrl} className="rounded-3xl" />
+  const src = props.imageUrl || 'https://firebasestorage.googleapis.com/v0/b/web3dev-bootcamp.appspot.com/o/courses_cover%2FSolana_NFTs.png?alt=media&token=fcdba884-7e66-46b8-9282-fced339907da'
+  return <Image width={350} height={350} src={src} className="rounded-3xl" />
 }
--- components/Home/index.js
+++ components/Home/index.js
@@ -4,19 +4,14 @@ import Cover from './Cover'
 import { useTranslation } from 'react-i18next'
 import RenderField from '../RenderField'
 export default function Main({ course }) {
-  const { t, i18n } = useTranslation()
+  const { t } = useTranslation()
   const ref = React.createRef()
-  const [link, setLink] = React.useState(null)
 
-  React.useMemo(() => {
-    if (course) {
-      setLink(`/courses/${course.id}`)
-    }
-  }, [course])
+  if (!course) return null
 
-  return course == null ? (
-    ''
-  ) : (
+  const link = `/courses/${course.id}`
+
+  return (
     <div className="z-0 mx-auto mb-14 mt-4 flex max-w-7xl flex-col items-center justify-between gap-0 px-8 lg:mt-4 lg:flex-row lg:gap-52">
       <div className="mx-auto flex flex-1 flex-col items-center">
         <div>
