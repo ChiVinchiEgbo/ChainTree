75a9beab29461b78b0747e47d298b44d8ca23d44
ChiVinchiEgbo <egbovincent005@gmail.com>
2026-07-28 19:05:18 -0400

    fix: add process.env.NEXT_PUBLIC_GTM_ID guard in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { CssBaseline } from '@nextui-org/react'
-import React from'react'
+import React from 'react'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -16,23 +16,27 @@ class MyDocument extends Document {
         <Head>
           {CssBaseline.flush()}
           <meta charSet="utf-8" />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');`,
-            }}
-          ></script>
+          {process.env.NEXT_PUBLIC_GTM_ID && (
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');`,
+              }}
+            />
+          )}
         </Head>
         <body>
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-            }}
-          ></noscript>
+          {process.env.NEXT_PUBLIC_GTM_ID && (
+            <noscript
+              dangerouslySetInnerHTML={{
+                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+              }}
+            />
+          )}
           <Main />
           <NextScript />
         </body>
