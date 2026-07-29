54131888ce2add114a8f9a55994b11ebc7c201de
ChiVinchiEgbo <egbovincent005@gmail.com>
2026-07-28 20:35:14 -0400

    feat: replace old course cover image with modern English Solidity banner

--- components/Home/Cover/index.js
+++ components/Home/Cover/index.js
@@ -2,6 +2,6 @@ import Image from 'next/image'
 import React from 'react'
 
 export default function Cover(props) {
-  const src = props.imageUrl || 'https://firebasestorage.googleapis.com/v0/b/web3dev-bootcamp.appspot.com/o/courses_cover%2FSolana_NFTs.png?alt=media&token=fcdba884-7e66-46b8-9282-fced339907da'
+  const src = props.imageUrl || '/assets/img/solidity_course_cover.png'
   return <Image width={350} height={350} src={src} className="rounded-3xl" unoptimized={true} alt="Course cover" />
 }
