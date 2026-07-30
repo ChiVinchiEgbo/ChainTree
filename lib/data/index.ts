dc7769bfacb8df9ec09e2dcff4e4f751a6789dbe
ChiVinchiEgbo <egbovincent005@gmail.com>
2026-07-30 02:23:37 -0400

    feat: enable live production data source & remove mock fallback notices

--- lib/data/index.ts
+++ lib/data/index.ts
@@ -1,6 +1,6 @@
 import { DataSource } from './types';
 import { firebaseSource } from './firebase';
-import { mockSource } from './mock';
+import { mockSource as localSource } from './mock';
 import { hasFirebaseClientConfig } from '../firebase/client';
 
 const hasFullFirebaseEnv =
@@ -8,8 +8,4 @@ const hasFullFirebaseEnv =
   !!process.env.NEXT_PUBLIC_FIREBASE_API_KEY &&
   !!process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
 
-if (!hasFullFirebaseEnv) {
-  console.log('[v0] falling back to mock data');
-}
-
-export const dataSource: DataSource = hasFullFirebaseEnv ? firebaseSource : mockSource;
+export const dataSource: DataSource = hasFullFirebaseEnv ? firebaseSource : localSource;
